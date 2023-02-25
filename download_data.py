055adc485c351bd43cb6fefa261d722e86e9b9f8
Maha OUALI <101869469+Mahaspace@users.noreply.github.com>
2023-02-25 15:58:27 +0100

    data

--- download_data.py
+++ download_data.py
@@ -1,11 +1,47 @@
 import pandas as pd
 import numpy as np
 import os
+import requests
 from sklearn.model_selection import train_test_split
 
 
-X = pd.read_csv(os.path.join('data', 'raw','X_train.csv'))
-Y = pd.read_csv(os.path.join('data', 'raw','Y_train.csv'))
+import os
+
+# file source:
+# https://www.kaggle.com/datasets/moussasacko/rakuten-france-multimodal-product-classification?resource=download
+url = ["https://drive.google.com/uc?export=download&id=1gUMr9ltyFB5P2fHsM-tlJfO3PWMxx25k"
+, "https://drive.google.com/uc?export=download&id=1Y3XLmowrwstA4OCgsvopnhuSDJBRBwH1"]
+
+
+
+public_path = "./data/public/"
+
+private_path = "./data/"
+
+file_names = ['X_train.csv', 'Y_train.csv'] 
+file_path = [os.path.join(private_path, file_name) for file_name in file_names]
+try:
+    os.stat(private_path)
+except OSError:
+    os.mkdir(private_path)
+
+try:
+    os.stat(public_path)
+except OSError:
+    os.mkdir(public_path)
+    
+print("Downloading the data files. This could take a couple of minutes...")   
+for i in range(len(url)):
+# Send an HTTP request to the download link
+    r = requests.get(url[i])
+# Save the response content to a file
+    open(file_names[i], 'wb').write(r.content)
+print("Files downloaded...")
+
+
+
+X = pd.read_csv(os.path.join('data','X_train.csv'))
+Y = pd.read_csv(os.path.join('data','Y_train.csv'))
 # 2 - Perform any data cleaning and split into private train/test subsets
 def split_data(X, Y, test_size=0.2, random_state=57):
     X_test, X_train, Y_test, Y_train = train_test_split(X, Y, test_size=test_size, random_state=random_state)
@@ -13,12 +49,10 @@ def split_data(X, Y, test_size=0.2, random_state=57):
 
 # specify the random_state to ensure reproducibility
 X_train, X_test, Y_train, Y_test = split_data(X, Y)
-# write a fucntion that join the input and output dataframes
+
 def join_data(X, Y):
      return pd.concat([X, Y], axis=1)
     
-# 3 - Split public train/test subsets. In this case the private training
-# data will be used as the public data
 df_public_train = join_data(X_train, Y_train)
 df_public_test = join_data(X_test, Y_test)
 
