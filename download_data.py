4044f85a4e558f9f21300ebb351195822bcab156
Maha OUALI <101869469+Mahaspace@users.noreply.github.com>
2023-02-25 13:34:23 +0100

    download_data.py

--- download_data.py
+++ download_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-from sklearn.model_selection import train_test_split# 1 - read in or download the data.
+from sklearn.model_selection import train_test_split
 
 
 X = pd.read_csv(os.path.join('data', 'X_train.csv'))
