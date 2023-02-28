607285320b3f29f41624fd5eebb050772268bd7d
Romain Louvet <romain.louvet@telecom-paris.fr>
2023-02-28 20:59:46 +0100

    adding a starting kit

---
+++ submissions/starting_kit/classifier.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+class Classifier(object):
+    def __init__(self):
+        pass
+
+    def fit(self, X_train, y):
+        classes = [
+            1,
+            4,
+            5,
+            6,
+            13,
+            114,
+            116,
+            118,
+            128,
+            132,
+            156,
+            192,
+            194,
+            206,
+            222,
+            228,
+            1281,
+            1301,
+            1302,
+            2403,
+            2462,
+            2522,
+            2582,
+            2583,
+            2585,
+            2705,
+            2905,
+        ]
+        
+        self.n_classes = len(classes)
+        pass
+
+    def predict(self, X_train):
+        
+        proba = np.random.rand(len(X_train), self.n_classes)
+        proba /= proba.sum(axis=1)[:, np.newaxis]
+
+        return proba
